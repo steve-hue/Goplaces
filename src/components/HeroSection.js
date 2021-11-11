b22f376a8ccc7671f32760448cfb960cac274f56
Steve Stagy <stevesk2001@gmail.com>
2021-11-11 23:11:20 +0400

    second commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection(){
     return(
         <div className="hero-container">
              
-             {/* <img src="/images/img-7.jpg" /> */}
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
+             <img src="https://raw.githubusercontent.com/briancodex/react-website-v1/starter/public/images/img-7.jpg" />
+            {/* <video src="/videos/video-2.mp4" autoPlay loop muted /> */}
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for</p>
             <div className="hero-btns">
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,5 +2,8 @@ import React from "react";
 import "../../App.css";
 
 export default function Services(){
-    return <h1 style={{color:"black"}} className="services">Services</h1>
+    return (<div>
+    <h1 style={{color:"black"}} className="services">Services</h1>
+    <img src="https://raw.githubusercontent.com/briancodex/react-website-v1/starter/public/images/img-1.jpg" alt="bv" />
+    </div>)
 }
